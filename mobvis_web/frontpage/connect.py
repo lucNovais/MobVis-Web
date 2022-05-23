14dbd0c61018d6741e373477afd3ebb6d6285bcd
lucNovais <lucasnovais222@gmail.com>
2022-05-23 20:29:24 -0300

    Starting to change the backend connection

--- mobvis_web/frontpage/connect.py
+++ mobvis_web/frontpage/connect.py
@@ -9,6 +9,24 @@ from mobvis.metrics.utils.MetricBuilder import MetricBuilder as mb
 from mobvis.plots.metric_plotter import *
 from mobvis.plots.spatial_plotter import *
 
+NEED_LOCATIONS = [
+    'TRVD',
+    'RADG',
+    'VISO',
+    'AVSP',
+    'TRVT',
+    'VIST'
+]
+
+NEED_HOMES = [
+    'RADG'
+]
+
+NEED_CONTACTS = [
+    'INCO',
+    'CODU'
+]
+
 def mobvis_connection(trace, configurations, metrics, plots):
     context = {}
     context['tables'] = []
@@ -16,89 +34,54 @@ def mobvis_connection(trace, configurations, metrics, plots):
 
     parsed_trace = par.parse(raw_trace=trace, is_ordered=True)
 
-    # TODO: Redo all of these procedures with more elegance
+    if metrics:
+        requirements = get_metrics_prerequirements(parsed_trace, metrics, configurations)
+        metrics_data = {}
+        for metric in metrics:
+            metrics_data[metric] = build_and_extract_metric(metric=metric, req_dict=requirements)
+        
+            context['tables'].append(metrics_data[metric].head(10).to_html())
 
-    [trace_loc, loc_centers] = loc.find_locations(
-        trace=parsed_trace,
-        max_d=float(configurations[0]),
-        pause_threshold=float(configurations[1]),
-        dist_type=configurations[3]
-    )
-
-    trace_homes = hloc.find_homes(trace_loc=trace_loc)
-
-    for metric in metrics:
-        if metric == 'TRVD':
-            trvd = mb.build_metric('TRVD', trace_loc=trace_loc, dist_type=configurations[3]).extract()
-            context['tables'].append(trvd.head(10).to_html(classes='tables_content', index=False))
-
-            if 'statistical' in plots:
-                fig = plot_metric_histogram(
-                    trvd,
-                    metric_name=metric,
-                    differ_nodes=False,
-                    show_title=True,
-                    show_y_label=True,
-                    nbins=30
-                )
-                context['figures'].append(fig.to_html())
-
-        if metric == 'RADG':
-            radg = mb.build_metric('RADG', trace=parsed_trace, trace_loc=trace_loc, sl_centers=loc_centers, homes=trace_homes, dist_type=configurations[3]).extract()
-            context['tables'].append(radg.head(10).to_html(classes='tables_content', index=False))
-
-            if 'statistical' in plots:
-                fig = plot_metric_histogram(
-                    radg,
-                    metric_name=metric,
-                    differ_nodes=False,
-                    show_title=True,
-                    show_y_label=True,
-                    nbins=30
-                )
-                context['figures'].append(fig.to_html())
-
-        if metric == 'TRVT':
-            trvt = mb.build_metric('TRVT', trace_loc=trace_loc).extract()
-            context['tables'].append(trvt.head(10).to_html(classes='tables_content', index=False))
-
-            if 'statistical' in plots:
-                fig = plot_metric_histogram(
-                    trvt,
-                    metric_name=metric,
-                    differ_nodes=False,
-                    show_title=True,
-                    show_y_label=True,
-                    nbins=30
-                )
-                context['figures'].append(fig.to_html())
-
-        if metric == 'VIST':
-            vist = mb.build_metric('VIST', trace_loc=trace_loc).extract()
-            context['tables'].append(vist.head(10).to_html(classes='tables_content', index=False))
-
-            if 'statistical' in plots:
-                fig = plot_metric_histogram(
-                    vist,
-                    metric_name=metric,
-                    differ_nodes=False,
-                    show_title=True,
-                    show_y_label=True,
-                    nbins=30
-                )
-                context['figures'].append(fig.to_html())
-
-        if metric == 'VISO':
-            viso = mb.build_metric('VISO', trace_loc=trace_loc).extract()
-            context['tables'].append(viso.head(10).to_html(classes='tables_content', index=False))
-
-            fig = plot_visit_order(
-                trace_viso=viso,
-                specific_users=[2]
-            )
-            context['figures'].append(fig.to_html())
-
-        if metric == 'INCO':
-            pass
 
     return context
+
+def get_metrics_prerequirements(trace, metrics, configurations):
+    requirements = {}
+
+    dist_type = configurations[3]
+    requirements['dist_type'] = dist_type
+
+    if [metric for metric in metrics if metric in NEED_LOCATIONS]:
+        max_d = float(configurations[0])
+        pause_t = float(configurations[1])
+
+        [trace_loc, sl_centers] = loc.find_locations(
+            trace=trace,
+            max_d=max_d,
+            pause_threshold=pause_t,
+            dist_type=dist_type
+        )
+
+        requirements['max_d'] = max_d
+        requirements['pause_t'] = pause_t
+        requirements['trace_loc'] = trace_loc
+        requirements['loc_centers'] = sl_centers
+
+        if [metric for metric in metrics if metric in NEED_HOMES]:
+            requirements['trace'] = trace
+            trace_homes = hloc.find_homes(trace_loc)
+            requirements['homes'] = trace_homes
+
+    if [metric for metric in metrics if metric in NEED_CONTACTS]:
+        contact_r = float(configurations[2])
+
+        contacts = cnt.detect_contacts(trace, contact_r, dist_type)
+
+        requirements['contacts_df'] = contacts
+
+    return requirements
+
+def build_and_extract_metric(metric, req_dict):
+    metric_data = mb.build_metric(metric, **req_dict).extract()
+
+    return metric_data
